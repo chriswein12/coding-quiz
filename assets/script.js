a27c79086c1999e28c933a037fd91616c47c316c
Chris Wein <chriswein12@yahoo.com>
2020-09-27 17:47:03 -0500

    added new elements for questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,30 @@
 var questions = [
     {
+        number: "Question 1",
         question: "Which of the following is not a logical operator?",
         answers: ["|| (OR)", "@@ (AS WELL AS)", "&& (AND)", "! (NOT)"],
         correct: "@@ (AS WELL AS)"
     },
     {
+        number: "Question 2",
         question: "Which of the following is not a variable type in JavaScript?",
         answers: ["reg", "var", "let", "const"],
         correct: "reg"
     },
     {
-
+        number: "Question 3",
         question: "Which of the following is the correct way to set up an array?",
         answers: ["var wine = ['Cabernet', 'Riesling', 'Sangiovese'];", "var wine = [Cabernet, Riesling, Sangiovese];", "var wine = {Cabernet; Riesling; Sangiovese};", "var wine = ('Cabernet', 'Riesling', 'Sangiovese');"],
         correct: "var wine = ['Cabernet', 'Riesling', 'Sangiovese']"
     },
     {
+        number: "Question 4",
         question: "var introduction = 'Hi!';. Which of the following will create a popup window saying 'Hi!'?",
         answers: ["console.log(introduction);", "console.dir(introduction);", "document.querySelector(introduction);", "alert(introduction);"],
         correct: "alert(introduction);"
     },
     {
+        number: "Question 5",
         question: "Which of the following is not a data type in JavaScript?",
         answers: ["String", "Number", "Letter", "Boolean"],
         correct: "Letter"
@@ -30,6 +34,7 @@ var questions = [
 var startBtn = document.querySelector("#start-button");
 var questionTitleEl = document.querySelector("#question-title-container")
 var questionBoxEl = document.querySelector("#question-box");
+var answersListEl = document.querySelector("answersList")
 var time = 75;
 var currentQuestionIndex = 0;
 
@@ -44,25 +49,35 @@ var startQuiz = function() {
     pullQuestions();
 }
 
-var pullQuestions = function(questions) {
+var pullQuestions = function() {
 
-    for (var i = 0; i < questions.length;) {
-
-
-   
+    currentQuestion = questions[currentQuestionIndex];
+       
     var questionTitleTextEl = document.createElement("h2");
     questionTitleTextEl.className = "question-title";
-    questionTitleTextEl.innerHTML = "<h2 class='question-title'>Question " + questions.id + "</h2>";
+    questionTitleTextEl.innerHTML = "<h2 class='question-title'>" + currentQuestion.number + "</h2>";
     questionTitleEl.appendChild(questionTitleTextEl);
 
-    var singleQuestionEl = document.createElement("p"); 
+    var questionContentEl = document.createElement("p");
+    questionContentEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    questionBoxEl.appendChild(questionContentEl);
 
-    currentQuestionIndex++
+    for (var i = 0; i < currentQuestion.answers.length; i++) {
+        var buttonChoiceEl = document.createElement("button");
+        buttonChoiceEl.textContent = currentQuestion.answers[i];
+        buttonChoiceEl.setAttribute()
+        questionBoxEl.appendChild(buttonChoiceEl);
 
 
     }
 
 }
+
+var answerHandler = function() {
+    
+
+}
+
 
 
 
@@ -88,3 +103,4 @@ var finalScore = function() {
 
 
 startBtn.addEventListener("click", startQuiz);
+answerBtn.addEventListener("click", answerHandler);
